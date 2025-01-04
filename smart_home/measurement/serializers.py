18d3dbc54d7d9c1a249d92518b8dab69af6cbbf7
Alejo <istominalexey@ro.ru>
2025-01-04 18:17:07 +0300

    serializers updated

--- smart_home/measurement/serializers.py
+++ smart_home/measurement/serializers.py
@@ -1,3 +1,22 @@
 from rest_framework import serializers
+from .models import Measurement, Sensor
 
-# TODO: опишите необходимые сериализаторы
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['name', 'description']
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['sensor', 'temperature', 'created_at']
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
